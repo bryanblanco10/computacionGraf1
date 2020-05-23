d36f94c0435c30035bc9d0f5d447b0e652efb0dd
dehoyos9804 <dehoyos9804@gmail.com>
2020-05-22 19:45:07 -0500

    añadiendo gitignore

--- src/Pintar.java
+++ src/Pintar.java
@@ -106,6 +106,13 @@ public class Pintar extends Applet implements KeyListener, Runnable{
         for(int i = 0; i < numero_iteraciones; i++){
             if(!enemigos.isEmpty()){
                 enemigos.get(i).trasladar(dx, dy);
+                
+                if(enemigos.get(i).getX()== 0){
+                    enemigos.remove(i);
+                }else{
+                    //detectar colisiones
+                    colisionaEnemigoPowerUp(i);
+                }
             }
             
             if(enemigos.size() < numero_iteraciones){
@@ -113,55 +120,20 @@ public class Pintar extends Applet implements KeyListener, Runnable{
             }
         }
     }
-
-    /*private void crearEnemigos(Graphics g) {
-
-        freezer = new Personaje("Freezer", "01-enemigo.png", 680, 90);
-        enemigos.add(freezer);
-        freezer.pintar(g, this);
-        //freezer.mensaje("Hola cacaroto soy " + freezer.getNombre());
-        esperar(500);
-        boo = new Personaje("Majin Boo", "02-enemigo.png ", 680, 260);
-        enemigos.add(boo);
-        boo.pintar(g, this);
-        //boo.mensaje("Hola cacaroto soy " + boo.getNombre());
-        esperar(500);
-        vegeta = new Personaje("Vegeta", "04-enemigo.png", 680, 430);
-        enemigos.add(vegeta);
-        vegeta.pintar(g, this);
-        //vegeta.mensaje("Hola cacaroto soy " + vegeta.getNombre() + "\n Estupido Sayayin");
-        esperar(500);
-        broly = new Personaje("Broly", "03-enemigo.png", 680, 590);
-        enemigos.add(broly);
-        broly.pintar(g, this);
-        //broly.mensaje("Hola cacaroto soy " + broly.getNombre());
-
-        int i = 680, desplazamiento = 50;
-
-        while (i > 150) {
-            limpiarPantalla(g);
-            pintarMundo(g);
-            crearHeroe(g);
-            
-            freezer.pintar(g, this);
-            freezer.setX(freezer.getX() - desplazamiento);
-            esperar(100);
-
-            boo.pintar(g, this);
-            boo.setX(boo.getX() - desplazamiento);
-            esperar(100);
-
-            vegeta.pintar(g, this);
-            vegeta.setX(vegeta.getX() - desplazamiento);
-            esperar(100);
-
-            broly.pintar(g, this);
-            broly.setX(broly.getX() - desplazamiento);
-            esperar(100);
-            i -= desplazamiento;
+    
+    public void colisionaEnemigoPowerUp(int indice){
+        try {
+            for(int i = 0; i < kameHa.size(); i++){
+                if(enemigos.get(indice).getRectangle().intersects(kameHa.get(i).getRectangle())){
+                    enemigos.remove(indice);//destruyo el enemigo
+                    kameHa.remove(i);//destruyo la bala de cañon   
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Han Ocurrido en la colision Enemigo <-> Cañones indice(" +indice +") " + e.getLocalizedMessage());
         }
+    }
 
-    }*/
 
     private void limpiarPantalla(Graphics g) {
         g.clearRect(0, 0, ANCHO_PANTALLA, ALTO_PANTALLA);
@@ -227,7 +199,7 @@ public class Pintar extends Applet implements KeyListener, Runnable{
                 break;
 
             case KeyEvent.VK_SPACE:
-                kameHa.add(new Personaje("Hame-Ha", "kame-ha.png", 50, goku.getCenter().getY()));
+                kameHa.add(new Personaje("Hame-Ha", "kame-ha.png", 40, goku.getCenter().getY()));
                 break;
 
             default:
